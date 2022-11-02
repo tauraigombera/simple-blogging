47050f9e69b391e059165f052737916d8bdf5e3d
TauG <tauraigombera@gmail.com>
2022-11-02 16:48:47 +0200

    istalled a log viewer

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
