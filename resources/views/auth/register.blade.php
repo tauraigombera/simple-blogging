00124fd092cc9a2ebe260985324c44aad9227d9d
TauG <tauraigombera@gmail.com>
2022-11-05 12:28:19 +0200

    modified register and login page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,180 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
-
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-
-                <x-input-error :messages="$errors->get('name')" class="mt-2" />
-            </div>
-
-            <!-- Username -->
-            <div class="mt-4">
-                <x-input-label for="username" :value="__('Username')" />
-
-                <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />
-
-                <x-input-error :messages="$errors->get('username')" class="mt-2" />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
-
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-
-                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+<x-layout>
+    <div class="min-h-screen sm:flex sm:flex-row mx-0 justify-center">
+        <div class="flex justify-center self-center  z-10">
+            <div class="px-6 py-6 bg-white mx-auto shadow-2xl rounded-2xl w-100 ">
+                <a href={{'/'}} class="float-right">
+                    <svg width="24" height="24" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M16.5 0.25C13.2861 0.25 10.1443 1.20305 7.47199 2.98862C4.79969 4.77419 2.71689 7.31209 1.48697 10.2814C0.257041 13.2507 -0.0647633 16.518 0.562247 19.6702C1.18926 22.8224 2.73692 25.7179 5.00952 27.9905C7.28213 30.2631 10.1776 31.8108 13.3298 32.4378C16.482 33.0648 19.7493 32.743 22.7186 31.513C25.6879 30.2831 28.2258 28.2003 30.0114 25.528C31.797 22.8557 32.75 19.7139 32.75 16.5C32.75 14.366 32.3297 12.2529 31.5131 10.2814C30.6964 8.30985 29.4994 6.51847 27.9905 5.00952C26.4815 3.50056 24.6902 2.3036 22.7186 1.48696C20.7471 0.670319 18.634 0.25 16.5 0.25ZM20.9038 18.5963C21.0561 18.7473 21.177 18.927 21.2595 19.1251C21.342 19.3231 21.3844 19.5355 21.3844 19.75C21.3844 19.9645 21.342 20.1769 21.2595 20.3749C21.177 20.573 21.0561 20.7527 20.9038 20.9038C20.7527 21.0561 20.573 21.177 20.3749 21.2594C20.1769 21.3419 19.9645 21.3844 19.75 21.3844C19.5355 21.3844 19.3231 21.3419 19.1251 21.2594C18.927 21.177 18.7473 21.0561 18.5963 20.9038L16.5 18.7913L14.4038 20.9038C14.2527 21.0561 14.073 21.177 13.8749 21.2594C13.6769 21.3419 13.4645 21.3844 13.25 21.3844C13.0355 21.3844 12.8231 21.3419 12.6251 21.2594C12.427 21.177 12.2473 21.0561 12.0963 20.9038C11.9439 20.7527 11.8231 20.573 11.7406 20.3749C11.6581 20.1769 11.6156 19.9645 11.6156 19.75C11.6156 19.5355 11.6581 19.3231 11.7406 19.1251C11.8231 18.927 11.9439 18.7473 12.0963 18.5963L14.2088 16.5L12.0963 14.4038C11.7903 14.0978 11.6184 13.6827 11.6184 13.25C11.6184 12.8173 11.7903 12.4022 12.0963 12.0963C12.4023 11.7903 12.8173 11.6184 13.25 11.6184C13.6827 11.6184 14.0978 11.7903 14.4038 12.0963L16.5 14.2088L18.5963 12.0963C18.9023 11.7903 19.3173 11.6184 19.75 11.6184C20.1827 11.6184 20.5978 11.7903 20.9038 12.0963C21.2098 12.4022 21.3817 12.8173 21.3817 13.25C21.3817 13.6827 21.2098 14.0978 20.9038 14.4038L18.7913 16.5L20.9038 18.5963Z"
+                            fill="#adb2bd" />
+                    </svg>
                 </a>
 
-                <x-primary-button class="ml-4">
-                    {{ __('Register') }}
-                </x-primary-button>
+                <div class="p-6">
+                    <div class="mb-4">
+                        <h3 class="font-semibold text-2xl text-gray-800">Join Simpleblog </h3>
+                    </div>
+                    <form method="POST" action="/register">
+                        @csrf
+                        <div class="space-y-5">
+                            <!-- Name -->
+                            <div class="space-y-2">
+                                <label for="name" class="text-base font-medium text-[#07074D] tracking-wide">Name</label>
+                                <input
+                                    class=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-orange"
+                                    type="text" name="name" id="name" value="{{old('name')}}" placeholder="Name" required autofocus >
+                                @error('name')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+                            <!-- Username -->
+                            <div class="space-y-2">
+                                <label for="username" class="text-base font-medium text-[#07074D] tracking-wide">Username</label>
+                                <input
+                                    class=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-orange"
+                                    type="text" name="username" id="username" value="{{old('username')}}" placeholder="Username" required>
+                                @error('username')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+                            <!-- Email -->
+                            <div class="space-y-2">
+                                <label for="email" class="text-base font-medium text-[#07074D] tracking-wide">Email</label>
+                                <input
+                                    class=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-orange"
+                                    type="email" name="email" id="email" value="{{old('email')}}" placeholder="Email" required>
+                                @error('email')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+                            <!-- Password -->
+                            <div class="space-y-2">
+                                <label for="password" class="mb-5 text-base font-medium text-[#07074D] tracking-wide">
+                                    Password
+                                </label>
+                                <input
+                                    class="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-orange"
+                                    type="password" name="password" id="password" placeholder="Enter your password" required>
+                                @error('password')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+                            <!-- Confirm Password -->
+                            <div class="space-y-2">
+                                <label for="password" class="mb-5 text-base font-medium text-[#07074D] tracking-wide">
+                                    Confirm Password
+                                </label>
+                                <input
+                                    class="w-full content-center text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-orange"
+                                    type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm your password" required>
+                                @error('password')
+                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+                            <!-- Already have an account-->
+                            <div class="flex items-center justify-between">
+                                <p class="text-center text-gray-500">Already have an account? <a href="/login" class="text-orange"> Sign in</a></p>
+                                <div>
+                                    <button type="submit"
+                                            class=" bg-orange hover:bg-lightOrange text-gray-100 p-3  rounded tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500">
+                                        Register
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
+                    <div class="pt-5 text-center text-gray-400 text-xs">
+                        <span>
+                            Copyright © 2022-2023
+                            <a rel="" target="_blank" title="SimpleBlog"
+                               class="text-green hover:text-green-500 ">SimpleBlog</a></span>
+                    </div>
+                </div>
+
             </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+    </div>
+</x-layout>
+
+{{--<x-guest-layout>--}}
+{{--    <x-auth-card>--}}
+{{--        <x-slot name="logo">--}}
+{{--            <a href="/">--}}
+{{--                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />--}}
+{{--            </a>--}}
+{{--        </x-slot>--}}
+
+{{--        <form method="POST" action="{{ route('register') }}">--}}
+{{--            @csrf--}}
+
+{{--            <!-- Name -->--}}
+{{--            <div>--}}
+{{--                <x-input-label for="name" :value="__('Name')" />--}}
+
+{{--                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />--}}
+
+{{--                <x-input-error :messages="$errors->get('name')" class="mt-2" />--}}
+{{--            </div>--}}
+
+{{--            <!-- Username -->--}}
+{{--            <div class="mt-4">--}}
+{{--                <x-input-label for="username" :value="__('Username')" />--}}
+
+{{--                <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />--}}
+
+{{--                <x-input-error :messages="$errors->get('username')" class="mt-2" />--}}
+{{--            </div>--}}
+
+{{--            <!-- Email Address -->--}}
+{{--            <div class="mt-4">--}}
+{{--                <x-input-label for="email" :value="__('Email')" />--}}
+
+{{--                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />--}}
+
+{{--                <x-input-error :messages="$errors->get('email')" class="mt-2" />--}}
+{{--            </div>--}}
+
+{{--            <!-- Password -->--}}
+{{--            <div class="mt-4">--}}
+{{--                <x-input-label for="password" :value="__('Password')" />--}}
+
+{{--                <x-text-input id="password" class="block mt-1 w-full"--}}
+{{--                                type="password"--}}
+{{--                                name="password"--}}
+{{--                                required autocomplete="new-password" />--}}
+
+{{--                <x-input-error :messages="$errors->get('password')" class="mt-2" />--}}
+{{--            </div>--}}
+
+{{--            <!-- Confirm Password -->--}}
+{{--            <div class="mt-4">--}}
+{{--                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />--}}
+
+{{--                <x-text-input id="password_confirmation" class="block mt-1 w-full"--}}
+{{--                                type="password"--}}
+{{--                                name="password_confirmation" required />--}}
+
+{{--                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />--}}
+{{--            </div>--}}
+
+{{--            <div class="flex items-center justify-end mt-4">--}}
+{{--                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">--}}
+{{--                    {{ __('Already registered?') }}--}}
+{{--                </a>--}}
+
+{{--                <x-primary-button class="ml-4">--}}
+{{--                    {{ __('Register') }}--}}
+{{--                </x-primary-button>--}}
+{{--            </div>--}}
+{{--        </form>--}}
+{{--    </x-auth-card>--}}
+{{--</x-guest-layout>--}}
 
 
 
