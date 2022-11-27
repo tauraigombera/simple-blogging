720afe2a55262bac35490d271d95d56b1b632961
TauG <tauraigombera@gmail.com>
2022-11-27 10:23:12 +0200

    UI bug fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,11 +8,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"/>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
-    <link rel="icon" href="{{ url('images/favicon.png') }}"/>
+    <!--<link rel="icon" href="{{ url('images/favicon.png') }}"/>-->
     <!-- production favicon link-->
     <link rel="icon" href="https://simple-blogging-ejgmo.ondigitalocean.app/images/favicon.png"/>
 
-    <link href="{{ URL::asset('css/styles.css') }}" rel="stylesheet"/>
+    <!--<link href="{{ URL::asset('css/styles.css') }}" rel="stylesheet"/>-->
     <!--production css link-->
     <link href="https://simple-blogging-ejgmo.ondigitalocean.app/build/assets/app.98b4b5a0.css" rel="stylesheet">
     @vite('resources/css/app.css')
@@ -24,7 +24,7 @@
 
 <x-flash />
 
-<script src="{{ URL::asset('js/script.js') }}" type="text/javascript"></script>
+<!-- <script src="{{ URL::asset('js/script.js') }}" type="text/javascript"></script>-->
 <!-- production js script-->
 <script src="https://simple-blogging-ejgmo.ondigitalocean.app/js/script.js"></script>
 
