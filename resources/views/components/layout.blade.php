fdcaa3f4e1b95c5d77c1b46c42f4da8c0acbb50d
TauG <tauraigombera@gmail.com>
2022-11-27 10:15:49 +0200

    UI bug fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"/>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
     <link rel="icon" href="{{ url('images/favicon.png') }}"/>
+    <!-- production css link-->
+    <link rel="icon" href="https://simple-blogging-ejgmo.ondigitalocean.app/images/favicon.png"/>
+
     <link href="{{ URL::asset('css/styles.css') }}" rel="stylesheet"/>
     @vite('resources/css/app.css')
     <title>SimpleBlog</title>
@@ -20,5 +23,8 @@
 <x-flash />
 
 <script src="{{ URL::asset('js/script.js') }}" type="text/javascript"></script>
+<!-- production js script-->
+<script src="https://simple-blogging-ejgmo.ondigitalocean.app/js/script.js"></script>
+
 </body>
 </html>
