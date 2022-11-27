b8300528af326b38aa9fa6d95c7902e5d5d58126
TauG <tauraigombera@gmail.com>
2022-11-27 09:42:03 +0200

    UI bug fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"/>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
-    <link rel="icon" href="{{ url('images/favicon.png') }}">
-    <link href="{{ URL::asset('css/styles.css') }}" rel="stylesheet">
+    <link rel="icon" href="{{ url('images/favicon.png') }}"/>
+    <link href="{{ URL::asset('css/styles.css') }}" rel="stylesheet"/>
     @vite('resources/css/app.css')
     <title>SimpleBlog</title>
 </head>
@@ -19,6 +19,6 @@
 
 <x-flash />
 
-<script type="text/javascript" src="{{ URL::asset('js/script.js') }}"></script>
+<script src="{{ URL::asset('js/script.js') }}" type="text/javascript"></script>
 </body>
 </html>
