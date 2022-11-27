ebe264f2ceb8d7080e28d7ec8f92a7b971479c48
TauG <tauraigombera@gmail.com>
2022-11-27 11:50:39 +0200

    UI bug fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,16 +9,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"/>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
     <link rel="icon" href="{{ url('images/favicon.png') }}"/>
-    <!-- production favicon link-->
-{{--    <link rel="icon" href="https://simple-blogging-ejgmo.ondigitalocean.app/images/favicon.png"/>--}}
 
-    <link href="{{ URL::asset('css/styles.css') }}" rel="stylesheet"/>
-    <!--production css link-->
-{{--    <link href="https://simple-blogging-ejgmo.ondigitalocean.app/css/styles.css"/>--}}
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet"/>
 
     @vite('resources/css/app.css')
-    <!--production vite link-->
-{{--    <link href="https://simple-blogging-ejgmo.ondigitalocean.app/build/assets/app.98b4b5a0.css" rel="stylesheet">--}}
 
     <title>SimpleBlog</title>
 </head>
@@ -28,9 +22,6 @@
 
 <x-flash />
 
-<script src="{{ URL::asset('js/script.js') }}" type="text/javascript"></script>
-<!-- production js script-->
-{{--<script src="https://simple-blogging-ejgmo.ondigitalocean.app/js/script.js"></script>--}}
-
+<script src="{{ asset('js/script.js') }}" type="text/javascript"></script>
 </body>
 </html>
