a023d64c6865afe14d93a6b14c81cd179bde89c6
TauG <tauraigombera@gmail.com>
2022-11-27 06:05:48 +0200

    UI bug fix for production

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,11 +8,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
-    <!-- <link rel="icon" href="{{ url('images/favicon.png') }}"> -->
-    <link rel="icon" href="https://simple-blogging-ejgmo.ondigitalocean.app/images/favicon.png">
+    <link rel="icon" href="{{ url('images/favicon.png') }}">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     @vite('resources/css/app.css')
-{{--    <link href="https://simple-blogging-ejgmo.ondigitalocean.app/build/assets/app.514c5084.css" rel="stylesheet">--}}
     <title>SimpleBlog</title>
 </head>
 <body>
